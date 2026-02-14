6dc18a38ce77760052a707396381e3368218e6a0
isipdagorbatkin-sudo <isip_d.a.gorbatkin@mpt.ru>
2026-02-14 09:01:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+/* function sum(a, b){
+    return a + b
+}
+
+console.log(sum(4,5))
+console.log(sum(-5,3))
+console.log(sum(1,9))
+
+function sayHello(){
+    let a = 22;
+    console.log ("HELLo")
+
+}
+
+sayHello()
+
+const sum = (a,b) => a + b */
+
+/* let numbers =  [1, 4, 4, -6, 4]
+let data = [10, "hello", true]
+
+console.log (numbers[0])
+
+numbers.push("hello")
+numbers.pop()
+
+let part = numbers.slice (1,3)
+
+let doubleNumbers = numbers.map((numbers) => numbers * 2)
+
+ let positiveNumbers = numbers.filter((numbers)numbers > 0)
+
+ let firstBigNumbers = numbers.find((numbers) => numbers>10 )
+
+ let sum = numbers.reduce(function(acc, curr) {
+    return acc+curr
+ }, 0) */
+
+ /* let user = {
+    name: "Anna",
+    age: 20,
+    isStudent: true,
+    greeting: function() {
+        console.log("Hello" + this.name)
+    }
+ }
+
+ console.log (user, greeting())
+ console.log(user.name)
+
+ let {name, age } = user */
+
+ 
